fb9a895b04e77fe1adc52422aed437d078ac6d71
Benjamin Dodgson <benji@tomorrow.me>
2020-01-19 16:54:34 -0800

    sending text message if we find user

--- src/cloud/main.js
+++ src/cloud/main.js
@@ -166,10 +166,28 @@ Parse.Cloud.define('verifyReservation', async request => {
   let reservation = await query.first({ useMasterKey: true });
 
   if (reservation) {
-    return reservation
+    if reservation.has('user') {
+
+      const userQuery = new Parse.Query(Parse.User);
+      userQuery.equalTo('objectId', reservation.user.objectId)
+      //Query for user
+      let user = await userQuery.first({ useMasterKey: true });
+
+      if (user) {
+        //Send user a verification code code 
+        twilioClient.messages.create({
+          body: `Your code for Benji is: ${ authCode }`,
+          from: '+12012560616',
+          to: user.phoneNumber,
+        });
+      }
+    } 
   } else {
     throw new Error('Reservation not found')
   }
+
+  return reservation
+
 }):
 
 // Parse.Cloud.define("auth", function(request,response) {
