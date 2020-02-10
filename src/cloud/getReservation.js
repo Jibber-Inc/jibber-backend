4e820875a15997f191817a7c3f4c4e44c118ba33
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-02-10 14:03:56 -0800

    comment out getReservation

--- src/cloud/getReservation.js
+++ src/cloud/getReservation.js
@@ -1,45 +1,45 @@
 
 
-/**
- * Query for reservation object
- */
-const getReservation = async (request, response) => {
-  const reservationId = request.params.reservationId;
+// /**
+//  * Query for reservation object
+//  */
+// const getReservation = async (request, response) => {
+//   const reservationId = request.params.reservationId;
 
-  // Build query
-  const reservationQuery = Parse.Query(Parse.Reservation);
+//   // Build query
+//   const reservationQuery = Parse.Query(Parse.Reservation);
 
-  // Eventually this should query by code
-  // @todo enforce code column unique values in db
-  reservationQuery.equalTo('objectId', reservationId);
+//   // Eventually this should query by code
+//   // @todo enforce code column unique values in db
+//   reservationQuery.equalTo('objectId', reservationId);
 
-  return reservationQuery
-    .first({ useMasterKey: true })
-    .then(async reservation => {
-      if (!!reservation) {
+//   return reservationQuery
+//     .first({ useMasterKey: true })
+//     .then(async reservation => {
+//       if (!!reservation) {
 
-        // If reservation already contains a user
-        if (reservation.has('user')) {
+//         // If reservation already contains a user
+//         if (reservation.has('user')) {
 
-          // Find the user
-          const userQuery = new Parse.Query(Parse.User);
-          userQuery.equalTo('objectId', reservation.user.objectId);
+//           // Find the user
+//           const userQuery = new Parse.Query(Parse.User);
+//           userQuery.equalTo('objectId', reservation.user.objectId);
 
-          //Query for user
-          const user = await userQuery.first({ useMasterKey: true });
+//           //Query for user
+//           // const user = await userQuery.first({ useMasterKey: true });
 
-          // Send the code to the phone number
-        }
+//           // Send the code to the phone number
+//         }
 
-        // If reservation has no user
-        // Return reservation in response
-      }
+//         // If reservation has no user
+//         // Return reservation in response
+//       }
 
-      // If reservation not found
-      // add phone number to waitlist
-      // return Some status code for client
+//       // If reservation not found
+//       // add phone number to waitlist
+//       // return Some status code for client
 
-    })
-}
+//     });
+// };
 
-export default getReservation;
+// export default getReservation;
