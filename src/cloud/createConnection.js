128b150aa3a1868d464c4966a4b5abbe9bb452d8
Benjamin Dodgson <benji@tomorrow.me>
2020-02-11 20:18:02 -0800

    Fixing query

--- src/cloud/createConnection.js
+++ src/cloud/createConnection.js
@@ -1,5 +1,4 @@
-import stripPhoneNumber from '../utils/stripPhoneNumber';
-
+import stripPhoneNumber from "../utils/stripPhoneNumber";
 
 /**
  * Create a connection
@@ -7,14 +6,13 @@ import stripPhoneNumber from '../utils/stripPhoneNumber';
  * @param {*} response
  */
 const createConnection = async (request, response) => {
-
   // The target users phone number
   let fromUser = request.user;
   let phoneNumber = request.params.phoneNumber;
 
   // Phone number is required in request body
   if (!phoneNumber) {
-    throw new Error('No phone number provided in request');
+    throw new Error("No phone number provided in request");
   }
 
   // Strip phone number
@@ -22,18 +20,17 @@ const createConnection = async (request, response) => {
 
   // Build query to find user with phoneNumber
   const userQuery = new Parse.Query(Parse.User);
-  userQuery.equalTo('phoneNumber', phoneNumber);
+  userQuery.equalTo("phoneNumber", phoneNumber);
 
   // Query for user
   let targetUser = await userQuery.first({ useMasterKey: true });
 
   // If target user found
   if (!!targetUser) {
-
     // Determine if the user already has a connection with the requesting user
-    const connectionQuery = new Parse.Query(Parse.Connection);
-    connectionQuery.equalTo('to', targetUser);
-    connectionQuery.equalTo('from', fromUser);
+    const connectionQuery = new Parse.Query("Connection");
+    connectionQuery.equalTo("to", targetUser);
+    connectionQuery.equalTo("from", fromUser);
 
     let connection = await connectionQuery.first({ useMasterKey: true });
 
@@ -44,22 +41,25 @@ const createConnection = async (request, response) => {
 
     // Otherwise create a connection between the users and set the status to invited
     const newConnection = new Parse.Connection();
-    newConnection.set('to', targetUser);
-    newConnection.set('from', fromUser);
-    newConnection.set('phoneNumber', phoneNumber);
-    newConnection.set('status', 'invited');
+    newConnection.set("to", targetUser);
+    newConnection.set("from", fromUser);
+    newConnection.set("phoneNumber", phoneNumber);
+    newConnection.set("status", "invited");
 
     return newConnection
       .save()
       .then(savedConnection => {
-
         // add the new connection to user making the request
-        fromUser.addUnique(newConnection, 'connections');
+        fromUser.addUnique(newConnection, "connections");
 
         return fromUser
           .save()
           .then(updatedFromUser => {
-            console.log(`[createConnection] Saved a new connection [${ newConnection.get('id') }] to user [${ updatedFromUser.get('id') }]`);
+            console.log(
+              `[createConnection] Saved a new connection [${newConnection.get(
+                "id"
+              )}] to user [${updatedFromUser.get("id")}]`
+            );
             return savedConnection;
           })
           .catch(error => response.error(error));
@@ -69,9 +69,9 @@ const createConnection = async (request, response) => {
 
   // If no target user found
   // Determine if their is already a connection with the given phoneNumber for the requesting user
-  const connectionQuery = new Parse.Query(Parse.Connection);
-  connectionQuery.equalTo('phoneNumber', phoneNumber);
-  connectionQuery.equalTo('from', fromUser);
+  const connectionQuery = new Parse.Query("Connection");
+  connectionQuery.equalTo("phoneNumber", phoneNumber);
+  connectionQuery.equalTo("from", fromUser);
 
   let connection = await connectionQuery.first({ useMasterKey: true });
 
@@ -82,24 +82,27 @@ const createConnection = async (request, response) => {
 
   // Otherwise create a connection with the sender and phoneNumber
   const newConnection = new Parse.Connection();
-  newConnection.set('from', fromUser);
-  newConnection.set('phoneNumber', phoneNumber);
-  newConnection.set('status', 'invited');
+  newConnection.set("from", fromUser);
+  newConnection.set("phoneNumber", phoneNumber);
+  newConnection.set("status", "invited");
 
   return newConnection
     .save()
     .then(savedConnection => {
-      fromUser.addUnique(newConnection, 'connections');
+      fromUser.addUnique(newConnection, "connections");
       return fromUser
         .save()
         .then(updatedFromUser => {
-          console.log(`[createConnection] Saved a new connection: ${ savedConnection.get('id') } to user: ${ updatedFromUser.get('id') }`);
+          console.log(
+            `[createConnection] Saved a new connection: ${savedConnection.get(
+              "id"
+            )} to user: ${updatedFromUser.get("id")}`
+          );
           return savedConnection;
         })
         .catch(error => response.error(error));
     })
     .catch(error => response.error(error));
 };
-
 
 export default createConnection;
