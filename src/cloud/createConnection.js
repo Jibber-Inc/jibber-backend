55e1b9618ac05a3aa150658361e1f73c95c5fdb2
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-02-23 03:43:19 -0800

    linter:fix

--- src/cloud/createConnection.js
+++ src/cloud/createConnection.js
@@ -1,4 +1,4 @@
-import stripPhoneNumber from "../utils/stripPhoneNumber";
+import stripPhoneNumber from '../utils/stripPhoneNumber';
 
 /**
  * Create a connection
@@ -12,17 +12,17 @@ const createConnection = async (request, response) => {
 
   // Phone number is required in request body
   if (!phoneNumber) {
-    throw new Error("No phone number provided in request");
+    throw new Error('No phone number provided in request');
   }
 
-  const Connection = Parse.Object.extend("Connection");
+  const Connection = Parse.Object.extend('Connection');
 
   // Strip phone number
   phoneNumber = stripPhoneNumber(phoneNumber);
 
   // Build query to find user with phoneNumber
   const userQuery = new Parse.Query(Parse.User);
-  userQuery.equalTo("phoneNumber", phoneNumber);
+  userQuery.equalTo('phoneNumber', phoneNumber);
 
   // Query for user
   let targetUser = await userQuery.first({ useMasterKey: true });
@@ -31,8 +31,8 @@ const createConnection = async (request, response) => {
   if (!!targetUser) {
     // Determine if the user already has a connection with the requesting user
     const connectionQuery = new Parse.Query(Connection);
-    connectionQuery.equalTo("to", targetUser);
-    connectionQuery.equalTo("from", fromUser);
+    connectionQuery.equalTo('to', targetUser);
+    connectionQuery.equalTo('from', fromUser);
 
     let connection = await connectionQuery.first({ useMasterKey: true });
 
@@ -43,24 +43,24 @@ const createConnection = async (request, response) => {
 
     // Otherwise create a connection between the users and set the status to invited
     const newConnection = new Connection();
-    newConnection.set("to", targetUser);
-    newConnection.set("from", fromUser);
-    newConnection.set("phoneNumber", phoneNumber);
-    newConnection.set("status", "invited");
+    newConnection.set('to', targetUser);
+    newConnection.set('from', fromUser);
+    newConnection.set('phoneNumber', phoneNumber);
+    newConnection.set('status', 'invited');
 
     return newConnection
       .save()
       .then(savedConnection => {
         // add the new connection to user making the request
-        fromUser.addUnique(newConnection, "connections");
+        fromUser.addUnique(newConnection, 'connections');
 
         return fromUser
           .save()
           .then(updatedFromUser => {
             console.log(
               `[createConnection] Saved a new connection [${newConnection.get(
-                "id"
-              )}] to user [${updatedFromUser.get("id")}]`
+                'id'
+              )}] to user [${updatedFromUser.get('id')}]`
             );
             return savedConnection;
           })
@@ -72,8 +72,8 @@ const createConnection = async (request, response) => {
   // If no target user found
   // Determine if their is already a connection with the given phoneNumber for the requesting user
   const connectionQuery = new Parse.Query(Connection);
-  connectionQuery.equalTo("phoneNumber", phoneNumber);
-  connectionQuery.equalTo("from", fromUser);
+  connectionQuery.equalTo('phoneNumber', phoneNumber);
+  connectionQuery.equalTo('from', fromUser);
 
   let connection = await connectionQuery.first({ useMasterKey: true });
 
@@ -84,21 +84,21 @@ const createConnection = async (request, response) => {
 
   // Otherwise create a connection with the sender and phoneNumber
   const newConnection = new Connection();
-  newConnection.set("from", fromUser);
-  newConnection.set("phoneNumber", phoneNumber);
-  newConnection.set("status", "invited");
+  newConnection.set('from', fromUser);
+  newConnection.set('phoneNumber', phoneNumber);
+  newConnection.set('status', 'invited');
 
   return newConnection
     .save()
     .then(savedConnection => {
-      fromUser.addUnique(newConnection, "connections");
+      fromUser.addUnique(newConnection, 'connections');
       return fromUser
         .save()
         .then(updatedFromUser => {
           console.log(
             `[createConnection] Saved a new connection: ${savedConnection.get(
-              "id"
-            )} to user: ${updatedFromUser.get("id")}`
+              'id'
+            )} to user: ${updatedFromUser.get('id')}`
           );
           return savedConnection;
         })
