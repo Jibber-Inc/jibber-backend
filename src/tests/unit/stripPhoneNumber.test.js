e1da70a86f6aec85075272feaa8552739777bf76
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-02-24 12:48:14 -0800

    parametrize stripPhoneNumber test

--- src/tests/unit/stripPhoneNumber.test.js
+++ src/tests/unit/stripPhoneNumber.test.js
@@ -4,6 +4,9 @@ import { ArgumentTypeError } from '../../errors';
 
 describe('test stripPhoneNumber', () => {
 
+  /**
+   * Should behave as expected
+   */
   test.each([
     ['+1-206-123-4567', '12061234567'],
     ['+1 (206) 123-4567', '12061234567'],
@@ -11,20 +14,29 @@ describe('test stripPhoneNumber', () => {
     ['206.123.4567', '2061234567'],
     ['☘️206--=123😎456🎲7', '2061234567'],
   ])(
-    'removes everything but valid numbers',
+    'given %p return %p',
     (input, expected) => {
       expect(stripPhoneNumber(input))
         .toEqual(expected);
     }
   );
 
-  it('should throw error if phone number type is not a string', () => {
-    expect(() => stripPhoneNumber(2061234567)).toThrow(ArgumentTypeError);
-    expect(() => stripPhoneNumber({ phone: '2061234567' })).toThrow(ArgumentTypeError);
-    expect(() => stripPhoneNumber()).toThrow(ArgumentTypeError);
-    expect(() => stripPhoneNumber(null)).toThrow(ArgumentTypeError);
-    expect(() => stripPhoneNumber(undefined)).toThrow(ArgumentTypeError);
-    expect(() => stripPhoneNumber([])).toThrow(ArgumentTypeError);
-  });
+  /**
+   * Should throw expected error if given invalid arguments
+   */
+  test.each([
+    [2061234567, ArgumentTypeError],
+    [206.1234567, ArgumentTypeError],
+    [{ phone: '2061234567' }, ArgumentTypeError],
+    [null, ArgumentTypeError],
+    [undefined, ArgumentTypeError],
+    [[], ArgumentTypeError],
+  ])(
+    'given %p should throw %p',
+    (input, expected) => {
+      expect(() => stripPhoneNumber(input)).toThrow(expected);
+    }
+  );
+
 });
 
