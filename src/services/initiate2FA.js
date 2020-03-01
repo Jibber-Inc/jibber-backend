76920f4cefbfc3d7a6fbf2f654b2bab6356b89cc
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-03-01 14:14:44 -0800

    update initiate2FA function

--- src/services/initiate2FA.js
+++ src/services/initiate2FA.js
@@ -1,16 +1,39 @@
+import Parse from '../providers/ParseProvider';
 import Twilio from '../providers/TwilioProvider';
+import ExtendableError from 'extendable-error-class';
 
+
+export class Inititate2FAError extends ExtendableError {}
 
 /**
  *
- * @param {String} auth_code
+ * @param {Number} auth_code
  * @param {Parse.User} user
  */
-const initiate2FA = (auth_code, user) => {
+export const initiate2FA = (auth_code, user) => {
+
+  if (!auth_code || typeof auth_code !== 'string') {
+    throw new Inititate2FAError(
+      '[HspaCMAg] Invalid auth_code argument'
+    );
+  }
+
+  if (!Boolean(user instanceof Parse.User)) {
+    throw new Inititate2FAError(
+      '[w395DBDy] Invalid user argument'
+    );
+  }
+
+  if (!user.get('phoneNumber')) {
+    throw new Inititate2FAError(
+      '[qV6Heiv8] Cannot initiate 2FA, no value in user.phoneNumber'
+    );
+  }
+
   return new Twilio().client.messages.create({
     body: `Your code for Benji is: ${ auth_code }`,
-    from: '+12012560616',
-    to: user.phoneNumber,
+    from: process.env.TWILIO_PHONE_NUMBER || '+12012560616',
+    to: user.get('phoneNumber'),
   });
 };
 
