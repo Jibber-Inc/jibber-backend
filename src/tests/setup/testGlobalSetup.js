bc58f8f791d3f3db1c5be32db12978b24ee1f600
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-03-01 10:03:26 -0800

    Add manageMongo step to global test setup

--- src/tests/setup/testGlobalSetup.js
+++ src/tests/setup/testGlobalSetup.js
@@ -1,29 +1,42 @@
 require('dotenv').config();
-
 const { setup: setupDevServer } = require('jest-dev-server');
+import tcp from 'tcp-port-used';
 import testTeardown from './testGlobalTeardown';
 import migrateMongo from './migrateMongo';
+import manageMongo from './manageMongo';
 import seedDB from './seedDB';
 import checkTwilioTestCredentials from './checkTwilioTestCredentials';
+
 
 /**
  * https://jestjs.io/docs/en/configuration#globalsetup-string
  */
 const globalSetup = async () => {
 
+  const PORT = process.env.PORT || 1337;
+
   // Spin up "jest-dev-server" to run during tests.
-  await setupDevServer({
-    command: 'npm run dev:src',
-    launchTimeout: 50000,
-    port: 1337,
-    debug: true,
-  })
+  await tcp.check(Number(PORT) , '127.0.0.1')
+    .then(is_in_use => {
+      if (is_in_use) {
+        throw new Error(
+          `[PDBV8IbN] Cannot start test run. Port ${ PORT } is in use.`
+        );
+      }
+    })
+    .then(() => setupDevServer({
+      command: 'npm run dev:src',
+      launchTimeout: 50000,
+      port: 1337,
+      debug: true,
+    }))
 
     // Integration tests connect to twilio and must use special "test" credentials.
     .then(checkTwilioTestCredentials)
 
-    // "Migrate" mongoDB from schemafiles
+    // "Migrate" mongoDB from schemafiles, update indexes, and seed db for test
     .then(migrateMongo)
+    .then(manageMongo)
     .then(seedDB)
 
     // Catch any error, then run the test teardown script.
