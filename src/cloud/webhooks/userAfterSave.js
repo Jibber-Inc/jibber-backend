795a34c9f28e41bb5ff685bba8e1e4be1e06e4fd
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-03-09 07:27:52 -0700

    Add some type checking to userAfterSave

--- src/cloud/webhooks/userAfterSave.js
+++ src/cloud/webhooks/userAfterSave.js
@@ -1,4 +1,11 @@
+import Parse from '../../providers/ParseProvider';
+import ExtendableError from 'extendable-error-class';
+import { isISO8601 } from 'validator';
+
 import createChatChannel from '../../utils/createChatChannel';
+
+
+class UserAfterSaveError extends ExtendableError {}
 
 
 /**
@@ -7,6 +14,22 @@ import createChatChannel from '../../utils/createChatChannel';
  */
 const userAfterSave = request => {
   const user = request.object;
+
+  if (!Boolean(user instanceof Parse.User)) {
+    throw new UserAfterSaveError(
+      '[c4V3VYAu] Expected user in request.object'
+    );
+  }
+  if (!isISO8601(user.createdAt)) {
+    throw new UserAfterSaveError(
+      '[hplRppBn] Expected user.createdAt to be ISO timestamp'
+    );
+  }
+  if (!isISO8601(user.updatedAt)) {
+    throw new UserAfterSaveError(
+      '[3Npvri9X] Expected user.updatedAt to be ISO timestamp'
+    );
+  }
 
   // Create new user chat channels
   // Since user.isNew() will always be false in the afterSave hook
