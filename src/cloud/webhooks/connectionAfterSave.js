15f930944bd526c8a6f9a389f25abd78a4717f2b
Benjamin Dodgson <benji@tomorrow.me>
2020-03-21 14:23:23 -0700

    adding an array of promises

--- src/cloud/webhooks/connectionAfterSave.js
+++ src/cloud/webhooks/connectionAfterSave.js
@@ -10,15 +10,23 @@ const connectionAfterSave = async request => {
   const connection = request.object; // eslint-disable-line no-unused-vars
 
   if (connection.isNew()) {
+    // Add save calls to the promises array
+    const promises = [];
+
     //Add the connection object to the "toUser" and "fromUser" connections array
+    if (!connection.get("toUser")) {
+      const toUser = connection.toUser;
+      toUser.addUnique("connections", connection);
+      promises.push(toUser.save());
+    }
 
-    const toUser = connection.toUser;
-    toUser.addUnique("connections", connection);
+    if (!connection.get("fromUser")) {
+      const fromUser = connection.fromUser;
+      fromUser.addUnique("connections", connection);
+      promises.push(fromUser.save());
+    }
 
-    const fromUser = connection.fromUser;
-    fromUser.addUnique("connections", connection);
-
-    Promise.all([fromUser.save(), toUser.save()]);
+    Promise.all(promises);
   }
 };
 
