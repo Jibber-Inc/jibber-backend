7f1f0ab560e816799b1a48ff020a8e5b749a5479
Benjamin Dodgson <benji@tomorrow.me>
2020-03-21 14:14:30 -0700

    saving the connection to the toUser and fromUser

--- src/cloud/webhooks/connectionAfterSave.js
+++ src/cloud/webhooks/connectionAfterSave.js
@@ -1,11 +1,25 @@
+import Parse from "../../providers/ParseProvider";
+
+class ConnectionAfterSaveError extends ExtendableError {}
 
 /**
  * After save webhook for Connection objects.
  * @param {Object} request
  */
-const connectionAfterSave = request => {
-  const connection = request.object;  // eslint-disable-line no-unused-vars
-};
+const connectionAfterSave = async request => {
+  const connection = request.object; // eslint-disable-line no-unused-vars
 
+  if (connection.isNew()) {
+    //Add the connection object to the "toUser" and "fromUser" connections array
+
+    const toUser = connection.toUser;
+    toUser.addUnique("connections", connection);
+
+    const fromUser = connection.fromUser;
+    fromUser.addUnique("connections", connection);
+
+    Promise.all([fromUser.save(), toUser.save()]);
+  }
+};
 
 export default connectionAfterSave;
