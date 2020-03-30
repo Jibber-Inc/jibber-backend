8daa48cbc8e82f11a57625600deb414bbf315d79
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-03-29 21:19:57 -0700

    we CAN check if user.existed() in afterSave method --> as opposed to user.isNew() in beforeSave method

--- src/cloud/webhooks/userAfterSave.js
+++ src/cloud/webhooks/userAfterSave.js
@@ -31,9 +31,7 @@ const userAfterSave = request => {
   }
 
   // Create new user chat channels
-  // Since user.isNew() will always be false in the afterSave hook
-  // we're comparing the createdAt/updatedAt timestamps to determine a new user
-  if (user.createdAt === user.updatedAt) {
+  if (!user.existed()) {
     Promise.all([
       createChatChannel(user, `Welcome_${user.id}`, 'Welcome!'),
       createChatChannel(user, `Feedback_${user.id}`, 'Feedback'),
