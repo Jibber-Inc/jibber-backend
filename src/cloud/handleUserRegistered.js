72a6672667ae7bd07909f732954430b020d74495
Benjamin Dodgson <benji@tomorrow.me>
2020-04-12 18:38:06 -0700

    removing all seemingly unneeded code

--- src/cloud/handleUserRegistered.js
+++ src/cloud/handleUserRegistered.js
@@ -1,27 +1,17 @@
-// import Parse from '../providers/ParseProvider';
-// import ExtendableError from 'extendable-error-class';
 
-// import createChatChannelService from '../../services/createChatChannelService';
+import createChatChannelService from '../../services/createChatChannelService';
 
-// class HandleUserRegisteredError extends ExtendableError {}
+const handleUserRegistered = request => {
 
-// const handleUserRegistered = request => {
+  const user = request.user;
 
-//   const user = request.user;
-
-//   if (!Boolean(user instanceof Parse.User)) {
-//     throw new HandleUserRegisteredError(
-//       '[q3TZJ1y2] request.user is invalid.'
-//     );
-//   }
-
-//   // create channels
-//   Promise.all([
-//     createChatChannelService(user, `Welcome_${user.id}`, 'Welcome!'),
-//   ]);
-// };
+  // create channels
+  Promise.all([
+    createChatChannelService(user, `Welcome_${user.id}`, 'Welcome!'),
+  ]);
+};
 
 
 
 
-//  export default handleUserRegistered;
+ export default handleUserRegistered;
