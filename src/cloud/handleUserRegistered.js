a5e9ee2c454dc88416ca2483f94ef4c7e90c3b1e
Benjamin Dodgson <benji@tomorrow.me>
2020-04-12 18:47:28 -0700

    trying again

--- src/cloud/handleUserRegistered.js
+++ src/cloud/handleUserRegistered.js
@@ -1,9 +1,19 @@
+import Parse from '../providers/ParseProvider';
+import ExtendableError from 'extendable-error-class';
 
-import createChatChannelService from '../../services/createChatChannelService';
+import createChatChannelService from '../services/createChatChannelService';
+
+class HandleUserRegisteredError extends ExtendableError {}
 
 const handleUserRegistered = request => {
 
   const user = request.user;
+
+  if (!Boolean(user instanceof Parse.User)) {
+    throw new HandleUserRegisteredError(
+      '[q3TZJ1y2] request.user is invalid.'
+    );
+  }
 
   // create channels
   Promise.all([
