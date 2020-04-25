c0bbefdd820839c115a91cb3daff4e1a3d8f2f6e
Santiago Semhan <santiagosemhan@gmail.com>
2020-04-24 22:07:45 -0300

    Remove reinitialization

--- src/providers/ParseProvider.js
+++ src/providers/ParseProvider.js
@@ -1,8 +1,3 @@
-import Parse from 'parse/node';
-
-const { APP_ID, JAVASCRIPT_KEY, MASTER_KEY, SERVER_URL } = process.env;
-
-Parse.initialize(APP_ID, JAVASCRIPT_KEY, MASTER_KEY);
-Parse.serverURL = SERVER_URL;
+const Parse = global.Parse;
 
 export default Parse;
