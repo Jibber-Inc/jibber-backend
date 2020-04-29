2d9a31a0581c91e6791a3a82c8608cc9a14aada0
Santiago Semhan <santiagosemhan@gmail.com>
2020-04-29 18:02:14 -0300

    protect hashcode from parse User response

--- src/api.js
+++ src/api.js
@@ -39,6 +39,11 @@ const api = new ParseServer({
     // List of classes to support for query subscriptions
     classNames: ['Posts', 'Comments'],
   },
+  protectedFields: {
+    User: {
+      '*': ['hashcode'],
+    },
+  },
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
