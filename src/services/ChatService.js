ad52d5b861f6c81aa5ae5369baff2da0faf097ec
Santiago Semhan <santiagosemhan@gmail.com>
2020-05-04 16:30:27 -0300

    Replace Twilio Library on ChatService.

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -1,52 +1,9 @@
 import ExtendableError from 'extendable-error-class';
-const Chat = require('twilio-chat');
-const twilio = require('twilio');
-const AccessToken = twilio.jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
+import Twilio from '../providers/TwilioProvider';
 
 export class ChatServiceError extends ExtendableError {}
 
-/**
- * createChatToken utility function
- * @param {String} userId
- * @return {String} Jason Web Token
- */
-const createChatToken = userId => {
-  const {
-    TWILIO_ACCOUNT_SID,
-    TWILIO_API_KEY,
-    TWILIO_API_SECRET,
-    TWILIO_SERVICE_SID,
-  } = process.env;
-
-  if (!TWILIO_ACCOUNT_SID) {
-    throw new ChatServiceError('[Hr5B+AnF] expected TWILIO_ACCOUNT_SID');
-  }
-  if (!TWILIO_API_KEY) {
-    throw new ChatServiceError('[Ot/hJQ6Q] expected TWILIO_API_KEY');
-  }
-  if (!TWILIO_API_SECRET) {
-    throw new ChatServiceError('[oa55glNj] expected TWILIO_API_SECRET');
-  }
-  if (!TWILIO_SERVICE_SID) {
-    throw new ChatServiceError('[DWLPvwsL] expected TWILIO_SERVICE_SID');
-  }
-  if (!userId || typeof userId !== 'string') {
-    throw new ChatServiceError('[KV2wmxfD] expected userId');
-  }
-
-  const accessToken = new AccessToken(
-    TWILIO_ACCOUNT_SID,
-    TWILIO_API_KEY,
-    TWILIO_API_SECRET,
-  );
-  const chatGrant = new ChatGrant({
-    serviceSid: TWILIO_SERVICE_SID,
-  });
-  accessToken.addGrant(chatGrant);
-  accessToken.identity = userId;
-  return accessToken.toJwt();
-};
+const SERVICE_ID = process.env.TWILIO_SERVICE_SID;
 
 /**
  * Create a chat channel
@@ -61,7 +18,7 @@ const createChatChannel = async (
   owner,
   uniqueName,
   friendlyName,
-  isPrivate = false,
+  type = 'private',
   attributes = {},
 ) => {
   if (!owner) {
@@ -75,18 +32,20 @@ const createChatChannel = async (
   if (!friendlyName || typeof friendlyName !== 'string') {
     throw new ChatServiceError('[VNFMyXuf] friendlyName is required');
   }
-
-  // Make chat client
-  const token = createChatToken(owner.id);
-
   try {
-    const client = await Chat.Client.create(token);
-    return client.createChannel({
-      uniqueName,
-      friendlyName,
-      isPrivate,
-      attributes,
-    });
+    await new Twilio().client.chat
+      .services(SERVICE_ID)
+      .channels('unique')
+      .remove();
+    const channel = await new Twilio().client.chat
+      .services(SERVICE_ID)
+      .channels.create({
+        uniqueName,
+        friendlyName,
+        type,
+        attributes,
+      });
+    return channel;
   } catch (error) {
     throw new ChatServiceError(error.message);
   }
@@ -95,21 +54,43 @@ const createChatChannel = async (
 /**
  * Invite Members to a given channel
  *
- * @param {Twilio.Channel.uniqueName} uniqueName
+ * @param {String} channelSid
  * @param {Array<String>} members
  *
  * @returns {Promise}
  */
-const inviteMembers = async (uniqueName, members = []) => {
+const inviteMembers = async (channelSid, members = []) => {
   try {
-    const channel = await Chat.Client.getChannelByUniqueName(uniqueName);
-    await Promise.all(members.map(mId => channel.invite(mId)));
+    return members.map(mId =>
+      new Twilio().client.chat
+        .services(SERVICE_ID)
+        .channels(channelSid)
+        .invites.create({ identity: mId }),
+    );
   } catch (error) {
     throw new ChatServiceError(error.message);
   }
 };
 
+/**
+ * Invite Members to a given channel
+ *
+ * @param {String} channelSid
+ * @param {Array<String>} members
+ *
+ * @returns {Promise}
+ */
+const addMembersToChannel = async (channelSid, members = []) => {
+  return members.map(mId =>
+    new Twilio().client.chat
+      .services(SERVICE_ID)
+      .channels(channelSid)
+      .members.create({ identity: mId }),
+  );
+};
+
 export default {
   createChatChannel,
   inviteMembers,
+  addMembersToChannel,
 };
