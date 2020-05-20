dd4c37d21c9dac7f93109bf45177fba8a8a6f103
Santiago Semhan <santiagosemhan@gmail.com>
2020-05-20 13:52:01 -0300

    Implemented 'joined message' onMemberAdded

--- src/chatEventWebhooks/chatAfterEvent/onMemberAdded.js
+++ src/chatEventWebhooks/chatAfterEvent/onMemberAdded.js
@@ -1,3 +1,5 @@
+import ChatService from '../../services/ChatService';
+
 /**
  * EventType - string - Always onMemberAdded
  * MemberSid - string - The Member SID of the newly added Member
@@ -9,8 +11,28 @@
  *        - The reason for the addition of the member. Could be ADDED or JOINED
  * DateCreated - date string - The date of Member addition
  */
-const onMemberAdded = (request, response) => {
-  return response.status(200).json({});
+const onMemberAdded = async (request, response) => {
+  try {
+    let { ChannelSid, Identity } = request.body;
+    const channel = await ChatService.fetchChannel(ChannelSid);
+    const { createdBy } = channel;
+    let messageSid;
+    if (createdBy !== Identity) {
+      // Create message structure
+      const message = {
+        body: `[name](${Identity}) joined the conversation.`,
+        attributes: JSON.stringify({ context: 'status' }),
+        from: Identity,
+      };
+
+      // Send the message
+      const result = await ChatService.createMessage(message, ChannelSid);
+      messageSid = result.sid;
+    }
+    return response.status(200).json({ messageSid });
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
 };
 
 export default onMemberAdded;
