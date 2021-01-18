0efc1e3c748c88fa1c4081be454e9dd9e6bdfbd2
Santiago Semhan <santiagosemhan@gmail.com>
2021-01-18 15:43:25 -0300

    Support import json files.

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,16 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-strict-mode',
+    ['inline-json-import', {}],
   ],
   presets: [
     [
       '@babel/preset-env',
       {
-        'targets': {
-          'node': 'current'
-        }
+        targets: {
+          node: 'current',
+        },
       },
-    ]
+    ],
   ],
 };
