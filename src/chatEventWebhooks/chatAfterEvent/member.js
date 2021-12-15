ce5d422ba3c74b9764fc792756cfd63a8ae3828c
Guillermo Colotti <guillecolotti@Guillermos-MacBook-Pro.local>
2021-12-15 18:04:38 -0300

    feature: message when user joins or leaves a channel

--- src/chatEventWebhooks/chatAfterEvent/member.js
+++ src/chatEventWebhooks/chatAfterEvent/member.js
@@ -1,9 +1,37 @@
+import ChatService from "../../services/ChatService";
+import EventWrapper from '../../utils/eventWrapper';
+import UserUtils from '../../utils/userData';
+
 /**
+ * On member added webhook.
  *
  * @param {*} request
  * @param {*} response
  */
-const added = (request, response) => response.status(200).json();
+const added = async (request, response) => {
+  const {
+    conversationId,
+    user,
+  } = EventWrapper.getParams(request.body);
+  try {
+    const conversation = await ChatService.getConversationById(conversationId);
+    const fullName = UserUtils.getFullName(user);
+    const message = {
+      text: `${fullName} has joined the conversation.`,
+      type: 'system',
+      context: 'casual',
+      user_id: user.id,
+      attributes: JSON.stringify({
+        context: 'casual',
+      })
+    };
+    const messageCreated = await ChatService.createMessage(message, conversation);
+    return response.status(200).json(messageCreated);
+  } catch (error) {
+    console.warn('Error - member.added', error);
+    return response.status(500).json(error)
+  }
+};
 
 /**
  *
@@ -13,11 +41,35 @@ const added = (request, response) => response.status(200).json();
 const updated = (request, response) => response.status(200).json();
 
 /**
+ * On member added webhook.
  *
  * @param {*} request
  * @param {*} response
  */
-const removed = (request, response) => response.status(200).json();
+const removed = async (request, response) => {
+  const {
+    conversationId,
+    user,
+  } = EventWrapper.getParams(request.body);
+  try {
+    const conversation = await ChatService.getConversationById(conversationId);
+    const fullName = UserUtils.getFullName(user);
+    const message = {
+      text: `${fullName} has left the conversation.`,
+      type: 'system',
+      context: 'casual',
+      user_id: user.id,
+      attributes: JSON.stringify({
+        context: 'casual',
+      })
+    };
+    const messageCreated = await ChatService.createMessage(message, conversation);
+    return response.status(200).json(messageCreated);
+  } catch (error) {
+    console.warn('Error - member.added', error);
+    return response.status(500).json(error)
+  }
+};
 
 export default {
   added,
