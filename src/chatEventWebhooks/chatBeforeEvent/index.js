7fe90084c48d518e0c558a5625c7da5c81fb4e8f
Juan Blariza <juanblariza@gmail.com>
2022-01-27 16:01:05 -0300

    remove commented code

--- src/chatEventWebhooks/chatBeforeEvent/index.js
+++ src/chatEventWebhooks/chatBeforeEvent/index.js
@@ -5,34 +5,7 @@
  * @returns {Response}
  */
 const chatBeforeEvent = async () => {
-  /*
-  TYPE IS UNDEFINED BUT WE DON'T HAVE 
-  IMPLEMENTED THE MESSAGES HOOKS FOR CHAT BEFORE EVENT
-
-  */
-  // const { type } = request.body; //
- 
-  // const [currentHandler, eventType] = type && type.split('.');
- 
-  /* if (!currentHandler || !eventType) {
-    return response.status(500).json({ error: 'Webhook type is missing.' });
-  } */
-
-  /* const handlers = {
-    message,
-  }; */
-
-  // const eventLog = new Parse.Object('EventLog');
-  // try {
-    // await eventLog.save(null, { useMasterKey: true });
-
-  // return handlers[currentHandler][eventType](request, response);
-  // } catch (error) {
-  /*  const msg = `No handler found for ${type}`;
-      eventLog.set('error', msg);
-      await eventLog.save(null, { useMasterKey: true });
-      return response.status(500).json({ error: msg }); */
-   // }
+  
 };
 
 export default chatBeforeEvent;
