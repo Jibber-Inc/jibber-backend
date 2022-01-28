65a43dd67c0b7b5c1a1502cd5ce81fea62920a78
Juan Blariza <juanblariza@gmail.com>
2022-01-27 22:12:40 -0300

    feat(): change welcome and waitlist messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -18,19 +18,45 @@ const getMessage = (message, values) => {
 
 const messages = {
   welcome: [
-    'Hi %givenName% 👋, we are so glad you joined Jibber!',
-    'Jibber is all about communicating better with those you are closest too.',
-    "You may have noticed there isn't a send button! 😁 To send a message simply swipe up on the message bubble.",
-    'You can swipe up to REPLY or right to send a new message.',
-    'Tap a message to view all the replies.',
-    'Feel free to try it out and leave us any feedback here! Have fun 😎',
-  ],
+      "Invite friends or family. Groups of 3+ get in first.",
+      "How do I move up the waitlist?",
+      "100%. Jibber is not secretly owned by big social.",
+      "Are you an independent startup?",
+      "Users can soon. Accredited investors can now. JoinJibber.com",
+      "Can anyone invest in Jibber?",
+      "If we're good enough that 1 in 3 upgrade, we will never need ads.",
+      "How is your business model different?",
+      "Because they all work the same way. We think different can be better.",
+      "Why does the world need another chat app?",
+      "Yes. Plus many ways to earn a free upgrade.",
+      "Will there always be a free version?",
+      "We're thinking half of what it's worth, so it's twice the value.",
+      "What will premium cost?",
+      "Soon. Lots of premium features coming.",
+      "Can I create multiple groups?",
+      "Not yet. iPhone first for now.",
+      "Can I invite someone who has Android?",
+      "Yes. Up to 10 people, including you.",
+      "Can I use Jibber for group chat?",
+      "Messages sent respect the focus setting on your iPhone.",
+      "What does quiet or urgent delivery mean?",
+      "Yes, you can see how I felt when I wrote this. [😌 Calm]",
+      "Can I attach how I'm feeling to a message?",
+      "The darker means most recent message.",
+      "What do the colors mean?",
+      "So many things. Try scrolling.",
+      "What makes Jibber unique?",
+      "To improve the way we talk, so we connect when we're together.",
+      "Why did you make Jibber?",
+      "A new way to talk that feels more like being in person.",
+      "What is Jibber?"
+    ]
 };
 
 const waitlistMessages = [
-  "Hey %givenName% 👋, welcome to Jibber!",
-  "My name is Benji, and I'm one of the Co-Founders of Jibber.",
-  "If you ever wanna chat, you can just message me here."
+  "Hi, I'm Benji Dodgson, Co-founder of Jibber. Welcome to the private beta.",
+  "If you have any questions, I'm here for you. Just ask.",
+  "Type a message, then swipe it up to send it to me."
 ];
 
 export default {
