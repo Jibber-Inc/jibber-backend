bf7ceaf616213795ec3934dfd1307dafe8dde4f4
Juan Blariza <juanblariza@gmail.com>
2022-02-04 20:03:57 -0300

    feat(): implement typeform webhook

--- src/chatEventWebhooks/typeFormEvent/index.js
+++ src/chatEventWebhooks/typeFormEvent/index.js
@@ -9,18 +9,16 @@ import MessaginService from '../../services/MessagingService';
  */
 const typeFormEvent = async (request, response) => {
   const { form_response } = request.body;
-  console.log('AAAA');
+
   if (form_response && form_response.answers) {
     const { answers } = form_response;
-    console.log('BBBB');
+   
     if (answers) {
       const phoneNumber = answers[2].phone_number;
       const name = answers[0].text;
-
       const message = `${name} tap the link below to get access to the Jibber private alpha. https://testflight.apple.com/join/YnJTwvSL`;
-      console.log('SENDING.......')
+    
       await MessaginService.createMessage(phoneNumber, message);
-      console.log('SENT');
     }
   }
 };
