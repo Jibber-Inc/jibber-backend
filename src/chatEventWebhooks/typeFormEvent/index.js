03d6774fdd64e707c6b44d970120a9b3b956627b
Juan Blariza <juanblariza@gmail.com>
2022-02-04 17:38:59 -0300

    wip

--- src/chatEventWebhooks/typeFormEvent/index.js
+++ src/chatEventWebhooks/typeFormEvent/index.js
@@ -7,14 +7,7 @@
  */
 const typeFormEvent = async (request, response) => {
   console.log('***** ENTRO *****');
-
-  const { form_response } = response.body;
-  if (form_response) {
-    console.log('***** ANSWERS *****');
-    const { answers } = form_response;
-
-    console.log(answers[0]);
-  }
+  
 };
 
 export default typeFormEvent;
