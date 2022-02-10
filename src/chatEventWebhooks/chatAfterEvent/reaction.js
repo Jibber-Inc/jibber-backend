e12936d345c6a1150cd8ad78612a98d687172325
Juan Blariza <juanblariza@gmail.com>
2022-02-10 18:05:32 -0300

    feat() add console logs to test in heroku

--- src/chatEventWebhooks/chatAfterEvent/reaction.js
+++ src/chatEventWebhooks/chatAfterEvent/reaction.js
@@ -11,7 +11,11 @@ import { NOTIFICATION_TYPES } from '../../constants';
  * @param {*} response
  */
 const newReaction = async (request, response) => {
-  const {  message,  conversationCid,  reaction:incomingReaction } = EventWrapper.getParams(request.body);
+  const {
+    message,
+    conversationCid,
+    reaction: incomingReaction,
+  } = EventWrapper.getParams(request.body);
 
   const fromUser = await new Parse.Query(Parse.User).get(message.user.id);
   if (!fromUser) throw new Error('User not found!');
@@ -22,42 +26,48 @@ const newReaction = async (request, response) => {
   );
 
   if (incomingReaction.type === 'read') {
-  
-    const notice = await NoticeService.getNoticeByOwner(fromUser, NOTIFICATION_TYPES.UNREAD_MESSAGES);
-   
-    if(notice){
+    const notice = await NoticeService.getNoticeByOwner(
+      fromUser,
+      NOTIFICATION_TYPES.UNREAD_MESSAGES,
+    );
+
+    if (notice) {
       const attributes = notice.get('attributes');
-      const filteredAttributes = attributes.unreadMessageIds.filter(messageId => messageId !== message.id);
-  
+      const filteredAttributes = attributes.unreadMessageIds.filter(
+        messageId => messageId !== message.id,
+      );
+
       notice.set('attributes', {
         ...attributes,
-        unreadMessageIds: filteredAttributes
+        unreadMessageIds: filteredAttributes,
       });
-     
+
       notice.save(null, { useMasterKey: true });
     }
   }
 
-  if (reactionsFiltered.length && reactionsFiltered[0].user_id) {
-     const toUser = await new Parse.Query(Parse.User).get(
-     reactionsFiltered[0].user_id,
-  );
+  const {context} = JSON.parse(message.attributes);
+  console.log(context);
+  if (reactionsFiltered.length && reactionsFiltered[0].user_id && context === 'time-sensitive') {
+    const toUser = await new Parse.Query(Parse.User).get(
+      reactionsFiltered[0].user_id,
+    );
 
-  if (!toUser) throw new Error('No destination user found!');
+    if (!toUser) throw new Error('No destination user found!');
 
-  const fullName = UserUtils.getFullName(toUser);
-  
-  const data = {
-    messageId: message.id,
-    conversationCid,
-    title: `${fullName} read your message 🤓`,
-    body: `${fullName} read ${message.text} `,
-    target: 'conversation',
-    category: 'stream.chat',
-    interruptionLevel: 'passive',
-    threadId: conversationCid,
-    author: toUser.id,
-  };
+    const fullName = UserUtils.getFullName(toUser);
+
+    const data = {
+      messageId: message.id,
+      conversationCid,
+      title: `${fullName} read your message 🤓`,
+      body: `${fullName} read ${message.text} `,
+      target: 'conversation',
+      category: 'stream.chat',
+      interruptionLevel: 'passive',
+      threadId: conversationCid,
+      author: toUser.id,
+    };
 
     await PushService.sendPushNotificationToUsers(data, [fromUser]);
   }
