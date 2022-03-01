4491253c11ec9e18fd6c098df8429f63e5a9d5ff
Guillermo Colotti <gui.colotti@gmail.com>
2022-03-01 13:43:46 -0300

    re-add quePos logic

--- src/cloud/finalizeUserOnboarding.js
+++ src/cloud/finalizeUserOnboarding.js
@@ -4,59 +4,63 @@ import UserService from '../services/UserService';
 import Parse from '../providers/ParseProvider';
 // Constants
 import UserStatus from '../constants/userStatus';
+// Constants
+import { TRANSACTION } from '../constants/transactions';
 // Services
 import ChatService from '../services/ChatService';
 import PassService from '../services/PassService';
-import ReservationService, { ReservationServiceError } from '../services/ReservationService';
+import ReservationService from '../services/ReservationService';
 import NoticeService from '../services/NoticeService';
 // Notifications
 import { NOTIFICATION_TYPES } from '../constants';
 import TransactionService from '../services/TransactionService';
-// import CircleService from '../services/CircleService';
+import QuePositionsService from '../services/QuePositionsService';
+// Utils
+import db from '../utils/db';
 
 class FinalizeUserOnboardingError extends ExtendableError { }
 
-// Users that come with a reservation has full access
+// Users that come with a reservation have full access.
 // Users without a reservation are placed in a queue.
-// Their position in the queue is set when they send the validation code
+// Their position in the queue is set when they send the validation code.
 // The user status can be one of: active, inactive, waitlist
-// If the position is higher than the max allowed position (maxQuePosition), they get the waitlist status
 // Active: users that have full access to the application
-// Inactive: users that have full access to the application, but they didnt end the onboarding yet
+// Inactive: users that have full access to the application, but they didn't finish the onboarding yet
 // Waitlist: users in the Waitlist have to wait until the maxQuePosition is increased, letting more users get full access.
-const setUserStatus = async (user, reservation = null) => {
-  // TODO: Uncomment when we use again the currentQuePosition logic.
+// If the position is higher than the max allowed position (maxQuePosition), they get the waitlist status
+const setUserStatus = async (user, reservationId, passId) => {
   // Get the needed que values to calculate the user status
-  // const config = await Parse.Config.get({ useMasterKey: true });
+  const config = await Parse.Config.get({ useMasterKey: true });
   // get maxQuePosition from parse. This variable is manually set depending on the needs
-  // const maxQuePosition = config.get('maxQuePosition');
-  // get the last position of the queue + 1. For more information, check db import.
-  // let currentQuePosition = user.get('quePosition');
+  const maxQuePosition = config.get('maxQuePosition');
+  let currentQuePosition = user.get('quePosition');
 
-  // if (!currentQuePosition) {
-  //   currentQuePosition = await db.getValueForNextSequence('unclaimedPosition');
+  if (!currentQuePosition) {
+    // get the last position of the queue + 1. For more information, check db import.
+    currentQuePosition = await db.getValueForNextSequence('unclaimedPosition');
+    await QuePositionsService.update('unclaimedPosition', currentQuePosition);
+  }
 
-  //   await QuePositionsService.update('unclaimedPosition', currentQuePosition);
-  // }
+  user.set('quePosition', currentQuePosition);
 
-  if (user.get('status') && user.get('status') !== UserStatus.USER_STATUS_INACTIVE) {
-
-    if (reservation) {
+  if (user.get('status') && user.get('status') !== UserStatus.USER_STATUS_ACTIVE) {
+    if (reservationId || passId) {
       user.set('status', UserStatus.USER_STATUS_ACTIVE);
+    } else if (maxQuePosition >= currentQuePosition) {
+      user.set('status', UserStatus.USER_STATUS_INACTIVE);
     } else {
-      // TODO: Uncomment when we use again the currentQuePosition logic.
-      // user.set('quePosition', currentQuePosition);
-      // if (maxQuePosition >= currentQuePosition) {
-      //   user.set('status', 'inactive');
-      // } else {
-      //   user.set('status', 'waitlist');
-      // }
       user.set('status', UserStatus.USER_STATUS_WAITLIST);
     }
   }
+
+  return user;
 };
 
-
+/**
+ * 
+ * @param {*} user 
+ * @returns 
+ */
 const createInitialConversations = async (user) => {
   // Here we create the user in Stream
   const createdUser = await UserService.upsertUser({ id: user.id });
@@ -82,29 +86,32 @@ const finalizeUserOnboarding = async request => {
     }
 
     if (reservationId) {
-      user.set('status', UserStatus.USER_STATUS_INACTIVE);
       await ReservationService.handleReservation(reservationId, user);
-
     } else if (passId) {
-
-      user.set('status', UserStatus.USER_STATUS_INACTIVE);
       await PassService.handlePass(passId, user);
-    } else {
-      user.set('status', UserStatus.USER_STATUS_WAITLIST);
     }
 
-    const noticeData = {
-      type: NOTIFICATION_TYPES.UNREAD_MESSAGES,
-      body: 'You have 0 unread messages',
-      attributes: {
-        unreadMessages: []
-      },
-      priority: 1,
-      user
-    };
+    await setUserStatus(user, reservationId, passId);
 
-    // Create the Notice object
-    await NoticeService.createNotice(noticeData);
+    // Check if the user has a UNREAD_MESSAGES Notice
+    const notice = await new Parse.Query('Notice')
+      .equalTo('owner', user)
+      .equalTo('type', NOTIFICATION_TYPES.UNREAD_MESSAGES)
+      .first({ useMasterKey: true });
+    // If the user doesn't have a UNREAD_MESSASES Notice, create one
+    if (!notice) {
+      const noticeData = {
+        type: NOTIFICATION_TYPES.UNREAD_MESSAGES,
+        body: 'You have 0 unread messages',
+        attributes: {
+          unreadMessageIds: []
+        },
+        priority: 1,
+        user
+      };
+      // Create the Notice object
+      await NoticeService.createNotice(noticeData);
+    }
 
     // Hold on with this functionality
     // await CircleService.createCircle(user);
@@ -120,20 +127,23 @@ const finalizeUserOnboarding = async request => {
         break;
 
       default:
-        throw new FinalizeUserOnboardingError('');
+        break;
     }
 
-    await TransactionService.createInitialTransaction(user);
+    // Check if the user has a NEW_USER Transaction
+    const initialTransaction = await new Parse.Query('Transaction')
+      .equalTo('to', user)
+      .equalTo('eventType', TRANSACTION.EVENT_TYPE.NEW_USER)
+      .first({ useMasterKey: true });
+    // If not, create one
+    if (!initialTransaction) {
+      await TransactionService.createInitialTransaction(user);
+    }
 
     user.save(null, { useMasterKey: true });
 
     return user;
   } catch (error) {
-    if (error instanceof ReservationServiceError) {
-      setUserStatus(user);
-      user.save(null, { useMasterKey: true });
-      throw error;
-    }
     throw new FinalizeUserOnboardingError(error.message);
   }
 };
--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -1,7 +1,10 @@
 export const TRANSACTION = {
   INITIAL_NOTE: 'Welcome to Jibber',
+  EVENT_TYPE: {
+    NEW_USER: 'NEW_USER'
+  },
   TARGET: 'wallet',
-  CATEGORY: 'transaction'
+  CATEGORY: 'transaction',
 };
 
 export default TRANSACTION;
