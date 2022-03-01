4e787c1865b69f039ba44a9a8b3abde8263fc5cb
Guillermo Colotti <gui.colotti@gmail.com>
2022-03-01 16:02:23 -0300

    refactor initial transaction & add achievement constants

---
+++ src/constants/achievements.js
@@ -0,0 +1,70 @@
+export const ACHIEVEMENTS = {
+  joinJibber: {
+    bounty: 10,
+    name: "INTEREST_PAYMENT",
+    title: 'Join Jibber',
+    description: 'Earned for successfully joining Jibber.'
+  },
+  inviteSent: {
+    bounty: 5,
+    name: "INVITE_SENT",
+    title: 'Send Invite',
+    description: 'Earned for every invite sent.'
+  },
+  firstMessage: {
+    bounty: 1,
+    name: "FIRST_MESSAGE",
+    title: 'First Message',
+    description: 'Earned for sending your first message.'
+  },
+  firstFeeling: {
+    bounty: 1,
+    name: "FIRST_FEELING",
+    title: 'First Feeling',
+    description: 'Earned for sending your first feeling.'
+  },
+  firstUnreadMessage: {
+    bounty: 1,
+    name: "FIRST_UNREAD_MESSAGE",
+    title: 'Message Read',
+    description: 'Earned for reading your first message.'
+  },
+  firstGroup: {
+    bounty: 2,
+    name: "FIRST_GROUP",
+    title: 'First Group',
+    description: 'Earned for creating your first group.'
+  },
+  groupOfThreePlus: {
+    bounty: 2,
+    name: "GROUP_OF_PLUS",
+    title: 'First Group 3+',
+    description: 'Earned for joining your first group of 3+.'
+  },
+  bugBounty: {
+    bounty: 1,
+    name: "BUG_BOUNTY",
+    title: 'Bug Bounty',
+    description: 'Earned for reporting a bug. Just take a screen shot, and choose the option to send to Test Flight.'
+  },
+  featureRequest: {
+    bounty: 1,
+    name: "FEATURE_REQUEST",
+    title: 'Feature Request',
+    description: 'Earned for sharing a feature request.'
+  },
+  firstTenK: {
+    bounty: 10,
+    name: "FIRST_10K",
+    title: 'First 10,000',
+    description: 'Earned for being one of the first 10k users on Jibber.'
+  },
+  investor: {
+    bounty: 100,
+    name: "INVESTOR",
+    title: 'Invest in Jibber',
+    description: 'Earned for making an investment in Jibber. Add your email to learn more.'
+  },
+};
+
+export default ACHIEVEMENTS;
